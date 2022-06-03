3950eefd3f73e21c0ff959688d88485c55f10222
Wildannbotz <96801218+Wildanbotz@users.noreply.github.com>
2022-06-04 02:12:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
-global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
+global.owner = ['6285880486685','0']
+global.packname = 'Wilbotz'
+global.author = 'WILDAN'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'WilXd'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
